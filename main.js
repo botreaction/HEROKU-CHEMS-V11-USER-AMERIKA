18bda23855e97f1fdf1236c6440cf2b15545d919
Autoreadsw <159133841+Autoreadsw@users.noreply.github.com>
2024-03-12 13:01:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -470,7 +470,7 @@ XeonBotInc.sendContact = async (jid, kon, quoted = '', opts = {}) => {
 	XeonBotInc.sendMessage(jid, { contacts: { displayName: `${list.length} Contact`, contacts: list }, ...opts }, { quoted })
     }
 
-    XeonBotInc.public = true
+    XeonBotInc.public = false
 
     XeonBotInc.serializeM = (m) => smsg(XeonBotInc, m, store)
 
